c70ba56d2d65cb9e80ea86406f9237ddc1b9d243
Gaurav Singh <gauravlucifer20@gmail.com>
2025-03-01 14:58:39 +0530

    Resolved import errors

--- server/routes/assign.js
+++ server/routes/assign.js
@@ -1,19 +1,18 @@
 import express from "express";
-import prisma from "../prisma.js";
 import { auth, isAdmin } from "../middleware/auth.js";
-import { assignTest, getAllUsersOfATest } from "../controllers/assignController.js";
- 
-const router = express.Router();
- 
+import {
+  assignTest,
+  getAllUsersOfATest,
+} from "../controllers/assignController.js";
 
- 
+const router = express.Router();
+
 router.get("/test", (req, res) => {
   res.json({ message: "Assign route is working" });
 });
- 
 
 router.post("/", auth, isAdmin, assignTest);
-  
+
 router.get("/:testId", getAllUsersOfATest);
 
 export default router;
